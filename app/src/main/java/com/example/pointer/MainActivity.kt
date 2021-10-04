058290986609106a9c42328af301c8143b8a7f9c
macbro <eshmurodovdoston630@gmail.com>
2021-10-04 14:55:53 +0500

    Initial commit

---
+++ app/src/main/java/com/example/pointer/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.pointer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pointer.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
