31ca07572758b9b2a23333186f49b4e21aa704ba
Shahrizod-Developer <qosimovshaxrizod7376@gmail.com>
2021-10-08 18:05:10 +0500

    Create Lesson improved table

--- app/src/main/java/com/example/pointer/adapters/TableRecyclerAdapter.kt
+++ app/src/main/java/com/example/pointer/adapters/TableRecyclerAdapter.kt
@@ -1,11 +1,13 @@
 package com.example.pointer.adapters
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.example.pointer.R
 import com.example.pointer.models.Table
@@ -22,6 +24,7 @@ class TableRecyclerAdapter(var context: Context, var list: ArrayList<Table>): Re
         val roomNumber = itemView.findViewById<TextView>(R.id.room_number)
         val imageStudent = itemView.findViewById<ImageView>(R.id.image_student)
         val name = itemView.findViewById<TextView>(R.id.student_name)
+        val background = itemView.findViewById<ConstraintLayout>(R.id.back)
 
         fun onBind(table: Table)
         {
@@ -33,6 +36,8 @@ class TableRecyclerAdapter(var context: Context, var list: ArrayList<Table>): Re
                 roomNumber.text = table.room
                 table.image?.let { imageStudent.setImageResource(it) }
                 name.text = table.name
+
+
             }
 
         }
@@ -41,8 +46,20 @@ class TableRecyclerAdapter(var context: Context, var list: ArrayList<Table>): Re
         return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_course, parent, false))
     }
 
+    @SuppressLint("ResourceAsColor")
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.onBind(list[position])
+
+        if(position == 0)
+        {
+            holder.background.setBackgroundResource(R.color.secondary_color)
+        }
+        else{
+            holder.group.setTextColor(R.color.black)
+            holder.course.setTextColor(R.color.black)
+            holder.name.setTextColor(R.color.black)
+            holder.roomNumber.setTextColor(R.color.black)
+        }
     }
 
     override fun getItemCount() = list.size
