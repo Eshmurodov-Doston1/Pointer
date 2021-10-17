07b48129c7e99c9ee16c07ec1033979c8faf240b
khberv <khberv@gmail.com>
2021-10-18 00:35:25 +0500

    Added chat

---
+++ app/src/main/java/com/example/pointer/adapters/chat/ChatListAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.pointer.adapters.chat
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.pointer.R
+import com.example.pointer.databinding.LayoutItemChatListBinding
+import com.example.pointer.models.ChatListItem
+import com.example.pointer.utils.TimeEncoder
+
+class ChatListAdapter : RecyclerView.Adapter<ChatListAdapter.ChatListHolder>() {
+    val chatList: ArrayList<ChatListItem> = arrayListOf()
+
+    class ChatListHolder(item: View) : RecyclerView.ViewHolder(item) {
+        val binding = LayoutItemChatListBinding.bind(item)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatListHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.layout_item_chat_list, parent, false)
+        return ChatListHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ChatListHolder, position: Int) {
+        val item = chatList[position]
+
+        holder.binding.apply {
+            chatNameTxt.text = item.chatName
+            avaImg.setImageDrawable(item.avatar)
+            lastMsgTxt.text = item.lastMessage
+            lastMsgTime.text = TimeEncoder.dateToStr(item.lastMessageSent, "hh:mm").toString()
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return chatList.size
+    }
+}
